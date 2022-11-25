8b399b29b9dfb9fb22a3a256c298f6333adb3d2f
Ge Will <531sunlight@gmail.com>
2022-11-25 22:39:24 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Neumorphic",
     platforms: [
-        .iOS(.v13),.macOS(.v10_15)
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -27,5 +27,4 @@ let package = Package(
         .testTarget(
             name: "NeumorphicTests",
             dependencies: ["Neumorphic"]),
-    ]
-)
+    ])
